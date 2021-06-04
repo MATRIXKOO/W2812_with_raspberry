f372efc8a732114d5a1b879008e351ad5804a625
Crlwebby <1303965943@qq.com>
2021-06-04 23:25:03 +0800

    cursor pos

--- pick_color_v1.py
+++ pick_color_v1.py
@@ -1,6 +1,16 @@
-#pick color by win32 API
-#from https://blog.csdn.net/xugexuge/article/details/87537765
+'''pick color by win32 API
+reference:
+https://blog.csdn.net/xugexuge/article/details/87537765
+https://blog.csdn.net/weixin_34185320/article/details/91810157
+'''
 from ctypes import *
+import os,time
+import pyautogui as pag
+
+def get_pos():
+    x,y = pag.position()
+    return x,y
+    
 
 def get_color(x,y):
     gdi32 = windll.gdi32
@@ -14,4 +24,8 @@ def get_color(x,y):
     return [r,g,b]
 
 if __name__ == '__main__':
-    print(get_color(0,0))
+    while True:
+        x,y = get_pos()
+        print(get_color(x,y))
+        time.sleep(1)
+    print("ending")
